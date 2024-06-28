aff1ae15f2c4105d527f3cfb140b33579d48bf15
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-06-28 17:46:32 +0800

    set up clock

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,9 +8,10 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.clock = pygame.time.Clock()
 
-        # self.screen = pygame.display.set_mode((1200, 800))
-        self.screen = pygame.display.set_mode((600, 400))
+        self.screen = pygame.display.set_mode((1200, 800))
+        # self.screen = pygame.display.set_mode((600, 400))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
@@ -23,6 +24,7 @@ class AlienInvasion:
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+            self.clock.tick(60)
 
 
 if __name__ == '__main__':
