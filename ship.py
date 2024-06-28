49f46758b477c6b784567730502a97762e50ea9e
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-06-28 19:03:00 +0800

    make ship stay on screen

--- ship.py
+++ ship.py
@@ -24,15 +24,12 @@ class Ship:
         self.moving_right = False
         self.moving_left = False
 
-        # Ship settings
-        self.ship_speed = 1.5
-
     def update(self):
         """Update the ship's position based on movement flags."""
         # Update the ship's x value, not the rect.
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
         # Update rect object from self.x.
