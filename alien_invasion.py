d1b13eb0969a9d97957666f0b6a0459159348e99
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-06-28 18:50:29 +0800

    can move left

--- alien_invasion.py
+++ alien_invasion.py
@@ -49,9 +49,13 @@ class AlienInvasion:
                 if event.key == pygame.K_RIGHT:
                     # Move the ship to the right.
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
 
 if __name__ == '__main__':
