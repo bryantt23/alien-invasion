011c3c5f86cfdbe0fdeb52af7a66afd396a9c28e
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-06-30 03:43:16 +0800

    calculate score

--- alien_invasion.py
+++ alien_invasion.py
@@ -122,6 +122,11 @@ class AlienInvasion:
             self.bullets, self.aliens, True, True
         )
 
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points*len(aliens)
+            self.sb.prep_score()
+
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
@@ -196,6 +201,7 @@ class AlienInvasion:
 
             # Reset the game statistics.
             self.stats.reset_stats()
+            self.sb.prep_score()
             self.game_active = True
 
             # Get rid of any remaining bullets and aliens.
